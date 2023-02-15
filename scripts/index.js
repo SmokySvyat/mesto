f3e36ed8ccc53dc0b8612ab521a58decf8388d90
Svyat <swcnova@gmail.com>
2023-02-15 23:31:04 +0700

    added card__del, minor fix in popup.css, added ico images, edited script: added popup-add, slow appears, realise creating cardadded trash ico

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,80 @@
-let popup = document.querySelector('.popup');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+let popupEdit = document.querySelector('#popup-edit');
+let popupAdd = document.querySelector('#popup-add');
+
 let userName = document.querySelector('.profile__name');
 let userJob = document.querySelector('.profile__job');
-let form = document.querySelector('.popup-form');
-let edit = document.querySelector('.profile__edit');
-let close = document.querySelector('.popup__close');
+
+let editBtn = document.querySelector('.profile__edit');
+let addBtn = document.querySelector('.profile__add')
+let closeBtn = document.querySelector('.popup__close');
+
 let nameValue = document.querySelector('#name');
 let jobValue = document.querySelector('#job');
 
-function popupOpenClose() {
-    popup.classList.toggle('popup_active');
+let cardTemplate = document.getElementById('card').content;
+let placeName = document.querySelector('#place-name');
+let placeImg = document.querySelector('#place-img');
+let placeValue = popupAdd.querySelector('#place');
+let placeLinkValue = popupAdd.querySelector('#place-link');
+
+// let btnsLike = document.querySelectorAll('.card__like');
+
+// btnsLike.forEach((el) => {
+//   el.addEventListener('click', (e) => {
+//     let path = e.currentTarget.getAttribute('data-path');
+//     console.log(path);
+//   });
+// });
+
+function deleteCard() {}
+
+function popupAddOpenClose() {
+  popupAdd.classList.toggle('popup_active');
 }
 
-function handleFormSubmit(evt) {
+function addCard(evt) {
+  evt.preventDefault(evt);
+
+  let cards = document.querySelector('.cards');
+  let listItem = cardTemplate.querySelector('.card').cloneNode(true);
+  listItem.querySelector('#place-name').textContent = placeValue.value;
+  listItem.querySelector('#place-img').src = placeLinkValue.value;
+
+  cards.prepend(listItem);
+  popupAddOpenClose();
+}
+
+function popupEditOpenClose() {
+  popupEdit.classList.toggle('popup_active');
+}
+
+function handleFormSubmitEdit(evt) {
   evt.preventDefault(evt);
   let userNameNew = nameValue.value;
   let userJobNew = jobValue.value;
@@ -19,14 +82,16 @@ function handleFormSubmit(evt) {
   userName.textContent = userNameNew;
   userJob.textContent = userJobNew;
 
-  popupOpenClose();
+  popupEditOpenClose();
 }
 
-edit.addEventListener('click', () => {
+editBtn.addEventListener('click', () => {
   nameValue.value = userName.textContent;
   jobValue.value = userJob.textContent;
 
-  popupOpenClose();
+  popupEditOpenClose();
 });
-close.addEventListener('click', popupOpenClose);
-form.addEventListener('submit', handleFormSubmit);
+addBtn.addEventListener('click', popupAddOpenClose)
+closeBtn.addEventListener('click', popupEditOpenClose);
+popupEdit.addEventListener('submit', handleFormSubmitEdit);
+popupAdd.addEventListener('submit', addCard)
